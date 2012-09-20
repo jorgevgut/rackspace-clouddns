e31a381cbb725444328e579cc9d71edf199aff28
Jorge V <jogeavi914@gmail.com>
2012-09-19 21:46:50 -0500

    Update spec/domain_test_spec.rb

--- spec/domain_test_spec.rb
+++ spec/domain_test_spec.rb
@@ -6,10 +6,13 @@ describe 'Domain_test' do
     #creamos el cliente dns
     dns = CloudDns::Client.new(:username => 'jogeavi914', :api_key => '9d6c2204896497a4bcec4223227e936b', :location => :us)
     
+    puts dns 
     #creamos un dominio nuevo
-    domain = dns.create_domain('rubyspecs.com', :email => 'jogeavi914@gmail.com')
+    #domain = dns.create_domain('rubyspecs.com', :email => 'jogeavi914@gmail.com')
     
     #obtenemos la lista de los dominios
     domains = dns.domains
+    
+    puts domains
   end
 end
